066e0ab65c5523d636ba8c5e3e4b9d518a304937
lzw6666 <738186842@qq.com>
2017-07-02 23:26:58 +0800

    Initial commit

---
+++ app/src/main/java/com/lzw/sharesdk_2/App.java
@@ -0,0 +1,16 @@
+package com.lzw.sharesdk_2;
+
+import android.app.Application;
+
+/**
+ * Created by lzw on 2017/7/2.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        ShareSDK.initSDK(this);
+    }
+}
